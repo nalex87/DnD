51c43e6a217680725071f3cde77328423eebff66
Aleksey Nikolaenko <nalex87@gmail.com>
2022-07-14 17:51:36 -0700

    Initial Commit

---
+++ DnD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DnD
+//
+//  Created by Aleksey Nikolaenko on 14.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
